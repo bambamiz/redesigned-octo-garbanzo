e26ec12209fa774e46fa1ce1157dc9ab0f722840
bambamiz <vamvamiz91@gmail.com>
2022-08-23 16:39:23 -0400

    generated controller, and set index as root.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "guides#index"
+  
+  get "/guides", to: "guides#index"
 end
