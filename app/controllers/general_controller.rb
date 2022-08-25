5f40636fe3b4c2982e8a5c1d4756d7058e3f3cf4
bambamiz <vamvamiz91@gmail.com>
2022-08-24 23:49:18 -0400

    created and added the general.html.erb route to the controller

---
+++ app/controllers/general_controller.rb
@@ -0,0 +1,4 @@
+class GeneralController < ApplicationController
+    def general
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "guides#index"
   
   get "/guides", to: "guides#index"
+  get "/general", to: "general#general"
 end
