177a7eabc3c7553e2c0a2807a6907f163bb9ea1f
bambamiz <vamvamiz91@gmail.com>
2022-09-21 17:08:37 -0400

    added new images, added browsers_controller, finished the alt path to chromeadblock, added 3 new views for adblock (chrome, firefox, edge)

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,7 +1,2 @@
 class GuidesController < ApplicationController
-  def index
-  end
-
-  def display_settings
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   get "/guides", to: "guides#index"
   get "/display_settings", to: "guides#display_settings"
   get "/disabling_gamebar", to: "guides#disabling_gamebar"
-  get "/adding_adblock_chrome", to: "guides#adding_adblock_chrome"
+  get "/adding_adblock_chrome", to: "browsers#adding_adblock_chrome"
+  get "/adding_adblock_firefox", to: "browsers#adding_adblock_firefox"
+  get "/adding_adblock_edge", to: "browsers#adding_adblock_edge"
+  get "/adblock_chrome_alt", to: "browsers#adblock_chrome_alt"
 end
